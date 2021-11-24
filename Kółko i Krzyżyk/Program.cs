0a7faf734b3e9b8756be7883c15ac78b6246678a
mareo11 <mareo.pl@onet.pl>
2021-11-24 18:26:40 +0100

    Tic Tac Toe

---
+++ Kółko i Krzyżyk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kółko_i_Krzyżyk
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Kółko_I_Krzyżyk());
+        }
+    }
+}
