9fef8d6aa289b76d590c1579a1e71e6bc50463bd
mareo11 <mareo.pl@onet.pl>
2021-11-24 18:26:40 +0100

    Tic Tac Toe

---
+++ Kółko i Krzyżyk/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Kółko_i_Krzyżyk
+{
+    partial class Kółko_I_Krzyżyk
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.A1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nowaGraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaGraToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zasadyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oAutorzeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A1.Location = new System.Drawing.Point(27, 59);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(173, 150);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaGraToolStripMenuItem,
+            this.zamknijToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(145, 52);
+            // 
+            // nowaGraToolStripMenuItem
+            // 
+            this.nowaGraToolStripMenuItem.Name = "nowaGraToolStripMenuItem";
+            this.nowaGraToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.nowaGraToolStripMenuItem.Text = "Nowa Gra";
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.pomocToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(691, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaGraToolStripMenuItem1,
+            this.zamknijToolStripMenuItem1});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zasadyToolStripMenuItem,
+            this.oAutorzeToolStripMenuItem});
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.pomocToolStripMenuItem.Text = "Pomoc";
+            // 
+            // nowaGraToolStripMenuItem1
+            // 
+            this.nowaGraToolStripMenuItem1.Name = "nowaGraToolStripMenuItem1";
+            this.nowaGraToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.nowaGraToolStripMenuItem1.Text = "Nowa Gra";
+            this.nowaGraToolStripMenuItem1.Click += new System.EventHandler(this.nowaGraToolStripMenuItem1_Click);
+            // 
+            // zamknijToolStripMenuItem1
+            // 
+            this.zamknijToolStripMenuItem1.Name = "zamknijToolStripMenuItem1";
+            this.zamknijToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.zamknijToolStripMenuItem1.Text = "Zamknij";
+            this.zamknijToolStripMenuItem1.Click += new System.EventHandler(this.zamknijToolStripMenuItem1_Click);
+            // 
+            // zasadyToolStripMenuItem
+            // 
+            this.zasadyToolStripMenuItem.Name = "zasadyToolStripMenuItem";
+            this.zasadyToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.zasadyToolStripMenuItem.Text = "Zasady";
+            this.zasadyToolStripMenuItem.Click += new System.EventHandler(this.zasadyToolStripMenuItem_Click);
+            // 
+            // oAutorzeToolStripMenuItem
+            // 
+            this.oAutorzeToolStripMenuItem.Name = "oAutorzeToolStripMenuItem";
+            this.oAutorzeToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.oAutorzeToolStripMenuItem.Text = "O autorze";
+            this.oAutorzeToolStripMenuItem.Click += new System.EventHandler(this.oAutorzeToolStripMenuItem_Click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A2.Location = new System.Drawing.Point(259, 59);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(173, 150);
+            this.A2.TabIndex = 3;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A3.Location = new System.Drawing.Point(491, 59);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(173, 150);
+            this.A3.TabIndex = 4;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B1.Location = new System.Drawing.Point(27, 246);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(173, 150);
+            this.B1.TabIndex = 5;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B2.Location = new System.Drawing.Point(259, 246);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(173, 150);
+            this.B2.TabIndex = 6;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B3
+            // 
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B3.Location = new System.Drawing.Point(491, 246);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(173, 150);
+            this.B3.TabIndex = 7;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C3
+            // 
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C3.Location = new System.Drawing.Point(491, 428);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(173, 150);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C2.Location = new System.Drawing.Point(259, 428);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(173, 150);
+            this.C2.TabIndex = 9;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C1.Location = new System.Drawing.Point(27, 428);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(173, 150);
+            this.C1.TabIndex = 10;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Kółko_I_Krzyżyk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 599);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.A1);
+            this.Name = "Kółko_I_Krzyżyk";
+            this.Text = "Kółko i Krzyżyk";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nowaGraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowaGraToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zasadyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oAutorzeToolStripMenuItem;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+    }
+}
+
---
+++ Kółko i Krzyżyk/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kółko_i_Krzyżyk
+{
+    
+    public partial class Kółko_I_Krzyżyk : Form
+    {
+        bool tura = true;
+        int IlośćTur = 0;
+        public Kółko_I_Krzyżyk()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (tura)
+            {
+                btn.Text = "X";
+            }
+            else
+            {
+                btn.Text = "O";
+            }
+
+            tura = !tura;
+            btn.Enabled = false; //blokowanie przycisku już klikniętego
+            IlośćTur++;
+            Wygrana();
+        }
+
+        private void Wygrana()
+        {
+            bool zwycięsca = false;
+            
+            //Sprawdzanie w poziomie
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            {
+                zwycięsca = true;
+            }
+            else if ((B1.Text == B2.Text)&& (B2.Text == B3.Text) && (!B1.Enabled))
+            {
+                zwycięsca = true;
+            }
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                zwycięsca = true;
+            }
+           
+            //Sprawdzanie w pionie
+            if (A1.Text == B1.Text && B1.Text == C1.Text && (!A1.Enabled))
+            {
+                zwycięsca = true;
+            }
+            else if (A2.Text == B2.Text && B2.Text == C2.Text && !A2.Enabled)
+            {
+                zwycięsca = true;
+            }
+            else if (A3.Text == B3.Text && B3.Text == C3.Text && !A3.Enabled)
+            {
+                zwycięsca = true;
+            }
+
+            //Sprawdzanie po ukośie 
+
+            if (A1.Text == B2.Text && B2.Text == C3.Text && (!A1.Enabled))
+            {
+                zwycięsca = true;
+            }
+            else if (A3.Text == B2.Text && B2.Text == C1.Text && !A3.Enabled)
+            {
+                zwycięsca = true;
+            }
+          
+            String wygrał = "";
+            if (zwycięsca == true)
+            {
+                WyłączButton();
+
+                if (tura == true)
+                {
+                    wygrał = "O";
+                }
+                else
+                {
+                    wygrał = "X";
+                }
+
+                MessageBox.Show("Wygrał gracz :" + wygrał, "Zwycięstwo!!");
+            }
+            else
+            {
+                if(IlośćTur == 9)
+                {
+                    MessageBox.Show("Nikt nie wygrał.Remis!!", "Remis!!");
+                }
+
+            }
+        }
+        private void WyłączButton()
+        {
+            A1.Enabled = false;
+            
+            try
+            {
+                foreach(Control c in Controls)
+                {
+                    Button btn = (Button)c;
+                    btn.Enabled = false;
+
+                }
+            }
+            catch  { }
+
+        }
+
+        private void nowaGraToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            tura = true;
+            IlośćTur = 0;
+
+            A1.Enabled = true;
+            A1.Text = "";
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button btn = (Button)c;
+                    btn.Enabled = true;
+                    btn.Text = "";
+
+                }
+            }
+            catch { }
+            
+        }
+
+        private void zamknijToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void zasadyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rozpoczyna gracz X", "Zasady");
+        }
+
+        private void oAutorzeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kółko i Krzyżyk v1.0 by Mateusz","O Autorze");
+        }
+    }
+
+
+}
